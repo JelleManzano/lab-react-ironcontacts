3b42378a1cf7b4979e0ba6e0c8229ffbb9b3cd3c
Jelle <jmanzanohaagsma@gmail.com>
2022-11-01 20:20:17 +0100

    Finished all iterations, having some bugs when a contact repeats (shows 3 times and web does not respond anymore)

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,50 +1,93 @@
 import { useState } from "react";
 
-function Contacts ({allContacts}) {
-    const [contacts, setContacts] = useState(allContacts.slice(0,5))
-    
-    const addContact = () => {
-        const randomIndex = Math.floor(Math.random() * allContacts.length);
-        const randomContact = allContacts[randomIndex]
-        const copy = contacts.map((eachContact) => eachContact)
-        copy.push(randomContact)
-        setContacts(copy)
-    };
-    return (
-        <div>
-            <h1>IronContacts</h1>
-            <button onClick={addContact}>Add a Contact</button>
-            <button>Sort by name</button>
-            <button>sort by popularity</button>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Picture</th>
-                        <th>Name</th>
-                        <th>Popularity</th>
-                        <th>Won an Oscar</th>
-                        <th>Won an Emmy</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                 {contacts.map((contact) => (
-                    <tr key={contact.id}>
-                        <td>
-                            <img src={contact.pictureUrl} alt={contact.name} width="200px"/>
-                        </td>
-                        <td>{contact.name}</td>
-                        <td>{Math.floor(contact.popularity)}</td>
-                        <td>
-                            <button>Delete</button>
-                        </td>
-                    </tr>
-                 ))}
-                </tbody>
-            </table>
-        </div>
-    )
+function Contacts({ allContacts }) {
+  const [contacts, setContacts] = useState(allContacts.slice(0, 5));
+
+  const addContact = () => {
+    const randomIndex = Math.floor(Math.random() * allContacts.length);
+    const randomContact = allContacts[randomIndex];
+    const copy = contacts.map((eachContact) => eachContact);
+    copy.push(randomContact);
+    setContacts(copy);
+  };
+
+  const nameOrder = () => {
+    const copy = contacts.map((eachContact) => eachContact);
+
+    copy.sort((elem1, elem2) => {
+      if (elem1.name > elem2.name) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    setContacts(copy);
+  };
+
+  const popOrder = () => {
+    const copy = contacts.map((eachContact) => eachContact);
+
+    copy.sort((elem1, elem2) => {
+      if (elem1.popularity < elem2.popularity) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    setContacts(copy);
+  };
+
+  const deleteContact = (contactId) => {
+    const filteredContacts = contacts.filter((eachContact) => {
+        if(eachContact.id === contactId) {
+            return false
+        } else {
+            return true
+        }
+    })
+    setContacts(filteredContacts)
+  }
+
+  return (
+    <div>
+      <h1>IronContacts</h1>
+      <button onClick={addContact}>Add a Contact</button>
+      <button onClick={nameOrder}>Sort by name</button>
+      <button onClick={popOrder}>sort by popularity</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => (
+            <tr key={contact.id}>
+              <td>
+                <img
+                  src={contact.pictureUrl}
+                  alt={contact.name}
+                  width="200px"
+                />
+              </td>
+              <td>{contact.name}</td>
+              <td>{Math.floor(contact.popularity)}</td>
+              <td>{contact.wonOscar === true ? "🏆" : "" } </td>
+              <td>{contact.wonEmmy === true ? "🏆" : ""}</td>
+              <td>
+                <button onClick={() => deleteContact(contact.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-
-export default Contacts
+export default Contacts;
